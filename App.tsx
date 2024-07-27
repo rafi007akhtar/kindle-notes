91a03039d1e1d0e6656defa6d3cfbc3bdb8e454b
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-07-28 01:58:46 +0530

    fetching all tables in the main component and setting data based on the tables fetched

--- App.tsx
+++ App.tsx
@@ -8,10 +8,16 @@ import {
   MD3LightTheme as DefaultTheme,
   BottomNavigation,
 } from "react-native-paper";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SetupRoute from "./screens/SetupRoute";
 import { BaseRoute } from "react-native-paper/lib/typescript/components/BottomNavigation/BottomNavigation";
 import { StatusBar } from "expo-status-bar";
+import { closeDB, initDB, schema } from "./utils/db.util";
+import { ExpoSQLiteDatabase } from "drizzle-orm/expo-sqlite";
+import { useAtom } from "jotai";
+import { bookHighlightsAtom, fileContentsAtom } from "./data/fileData";
+import { convertHighlightsArrayToObject } from "./utils/highlights.util";
+import { NormalizedHighlights } from "./models/common.model";
 
 export default function App() {
   const [index, setIndex] = useState(0);
@@ -19,6 +25,54 @@ export default function App() {
     { key: "setup", title: "Setup", focusedIcon: "tune" },
     { key: "viewNotes", title: "View Notes", focusedIcon: "note-multiple" },
   ]);
+  const [fileContents, setFileContents] = useAtom(fileContentsAtom);
+  const [_, setBookHighlights] = useAtom(bookHighlightsAtom);
+
+  useEffect(() => {
+    (async () => {
+      const [drizzleDB, err] = await initDB();
+      if (err) {
+        console.error("Error in getting table:", err);
+        return;
+      }
+      console.log("no err in getting db");
+
+      const fileContentsTable = await (
+        drizzleDB as ExpoSQLiteDatabase<Record<string, never>>
+      )
+        .select()
+        .from(schema.clippingsFile);
+      console.log("should have gotten file contents");
+      console.log({ fileContentsTable });
+      if (fileContentsTable?.length) {
+        const fileContentsFromDB = fileContentsTable[0].fileContents || "";
+        console.log({ fileContentsFromDB });
+        if (fileContentsFromDB) {
+          setFileContents(fileContentsFromDB);
+        }
+        // console.log({ fileContents });
+      }
+
+      const bookHighlightsTable = await (
+        drizzleDB as ExpoSQLiteDatabase<Record<string, never>>
+      )
+        .select()
+        .from(schema.bookHighlights);
+      console.log({ bookHighlightsTable });
+      if (bookHighlightsTable?.length) {
+        const bookHighlights = convertHighlightsArrayToObject(
+          bookHighlightsTable as NormalizedHighlights
+        );
+        if (bookHighlights) {
+          setBookHighlights(bookHighlights);
+        }
+      }
+    })();
+
+    return () => {
+      closeDB();
+    };
+  }, []);
 
   const renderScene = BottomNavigation.SceneMap({
     setup: SetupRoute,
